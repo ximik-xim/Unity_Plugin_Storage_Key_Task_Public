0af2e196b0ec9e28f2725da95365d6fca1babb68
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-15 09:26:14 +0300

    - Добавление подержки TList(0.6.0)

--- TSG_Storage Key Task/Key/Key Task Data/TList/IdentifierAndData_TSG_KeyTaskData.cs
+++ TSG_Storage Key Task/Key/Key Task Data/TList/IdentifierAndData_TSG_KeyTaskData.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_TSG_KeyTaskData : AbsIdentifierAndData<IndifNameS
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<TSG_KeyTaskData>(json);
+ }
+#endif
 }
